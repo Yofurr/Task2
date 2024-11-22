dd80f0da0c40fe599eee4c731990d6f8a577ca8b
Yofurr <distrvlad@gmail.com>
2024-11-22 17:35:31 +0300

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,13 @@
+# Практическая работа по уроку "Динамическая типизация"
+
+Name='Дмитрий'
+print(Name)
+
+age=35
+print(age)
+new_age=3
+print(age+new_age)
+
+is_student=(age >= new_age)
+print(is_student)
+
